24a2a128407a54ff489e83a17fede710cf63d51f
Malhar Pandya <148015853+malharpandyaa@users.noreply.github.com>
2024-03-05 22:38:07 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let synth = new Tone.PolySynth(Tone.Synth);
+let bend = new Tone.PitchShift();
+
+bend.pitch = 0;
+synth.connect(bend);
+bend.toDestination();
+
+let notes = {
+  'a' : 'C4',
+  's' : 'D4',
+  'd' : 'E4',
+  'f' : 'F4',
+  'g' : 'G4',
+  'h' : 'A4',
+  'j' : 'B4',
+  'k' : 'C5'
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+pitchSlider = createSlider(-12, 12, 0, 0.1); //pitch down -12, pitch up 12, starting point is 0
+pitchSlider.position (120, 200);
+pitchSlider.mouseMoved(()=> bend.pitch = pitchSlider.value());
+
+}
+
+function keyPressed(){
+  let playNotes = notes[key];
+  synth.triggerAttack(playNotes);
+}
+
+function keyReleased(){
+  let playNotes = notes[key];
+  synth.triggerRelease(playNotes,'+0.03');
+}
+
+function draw() {
+  background(100, 220, 150);
+  text ('play A-K for synth', 140, 180)
+}
